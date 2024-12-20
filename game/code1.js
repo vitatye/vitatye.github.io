a4c21ede923eebb850c1ae2bbc74ed720a8ac25c
vitatye <110102@stu.hgsh.hc.edu.tw>
2024-12-20 12:20:28 +0800

    Add files via upload

---
+++ game/code1.js
@@ -0,0 +1,20 @@
+gdjs.yahCode = {};
+gdjs.yahCode.localVariables = [];
+
+
+gdjs.yahCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.yahCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.yahCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['yahCode'] = gdjs.yahCode;
---
+++ game/code2.js
@@ -0,0 +1,20 @@
+gdjs.dieCode = {};
+gdjs.dieCode.localVariables = [];
+
+
+gdjs.dieCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.dieCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.dieCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['dieCode'] = gdjs.dieCode;
